c984dfc99370da88470540be997496100dbcf72b
DingPengwei <www.dingpengwei@foxmail.com>
2019-11-23 23:36:14 +0800

    stash

--- src/views/organization/Created.vue
+++ src/views/organization/Created.vue
@@ -1,6 +1,24 @@
 <template>
-  <el-container>
-    Created
+  <el-container style="width:100%; text-align: center">
+    <el-form :model="createOrgForm" :rules="rules" >
+      <h3 class="title">{{title}}</h3>
+      <el-form-item prop="name">
+        <el-input type="text" v-model="createOrgForm.name" auto-complete="off" placeholder="请输入名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="slogan">
+        <el-input type="text" v-model="createOrgForm.slogan" auto-complete="off" placeholder="请输入口号"></el-input>
+      </el-form-item>
+      <el-form-item prop="description">
+        <el-input type="text" v-model="createOrgForm.description" auto-complete="off" placeholder="请输入描述"></el-input>
+      </el-form-item>
+      <el-form-item prop="logo">
+        <el-input type="text" v-model="createOrgForm.logo" auto-complete="off" placeholder="请上传logo"></el-input>
+      </el-form-item>
+
+      <el-form-item style="width:100%;">
+        <el-button type="primary" style="width:100%;" @click="">创建</el-button>
+      </el-form-item>
+    </el-form>
   </el-container>
 </template>
 
@@ -9,7 +27,19 @@
     name: 'Created',
     data () {
       return {
-        msg: 'Created'
+        title: '创建我的组织',
+        createOrgForm: {
+          name: 'name',
+          slogan: 'slogan',
+          description: 'description',
+          logo: 'logo',
+        },
+        rules: {
+          name: [{required: true, message: 'please enter your account', trigger: 'blur'}],
+          slogan: [{required: true, message: 'enter your password', trigger: 'blur'}],
+          description: [{required: true, message: 'enter your password', trigger: 'blur'}],
+          logo: [{required: true, message: 'enter your password', trigger: 'blur'}]
+        },
       }
     }
   }
--- src/views/organization/ReviewPending.vue
+++ src/views/organization/ReviewPending.vue
@@ -1,6 +1,56 @@
 <template>
   <el-container>
-    ReviewPending
+    <div class="block">
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in activities"
+          :key="index"
+          :icon="activity.icon"
+          :type="activity.type"
+          :color="activity.color"
+          :size="activity.size"
+          :timestamp="activity.timestamp">
+          {{activity.content}}
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/12" placement="top">
+          <el-card>
+            <h4>更新 Github 模板</h4>
+            <p>王小虎 提交于 2018/4/12 20:46</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/3" placement="top">
+          <el-card>
+            <h4>更新 Github 模板</h4>
+            <p>王小虎 提交于 2018/4/3 20:46</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/2" placement="top">
+          <el-card>
+            <h4>更新 Github 模板</h4>
+            <p>王小虎 提交于 2018/4/2 20:46</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/12" placement="top">
+          <el-card>
+            <h4>更新 Github 模板</h4>
+            <p>王小虎 提交于 2018/4/12 20:46</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/3" placement="top">
+          <el-card>
+            <h4>更新 Github 模板</h4>
+            <p>王小虎 提交于 2018/4/3 20:46</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/2" placement="top">
+          <el-card>
+            <h4>更新 Github 模板</h4>
+            <p>王小虎 提交于 2018/4/2 20:46</p>
+          </el-card>
+        </el-timeline-item>
+
+      </el-timeline>
+    </div>
   </el-container>
 </template>
 
@@ -9,7 +59,24 @@
     name: 'ReviewPending',
     data () {
       return {
-        msg: ''
+        activities: [{
+          content: '支持使用图标',
+          timestamp: '2018-04-12 20:46',
+          size: 'large',
+          type: 'primary',
+          icon: 'el-icon-more'
+        }, {
+          content: '支持自定义颜色',
+          timestamp: '2018-04-03 20:46',
+          color: '#0bbd87'
+        }, {
+          content: '支持自定义尺寸',
+          timestamp: '2018-04-03 20:46',
+          size: 'large'
+        }, {
+          content: '默认样式的节点',
+          timestamp: '2018-04-03 20:46'
+        }]
       }
     }
   }
